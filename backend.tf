16de1f940068293d0a03284f72be54f79c1beaf9
snort12 <118768699+snort12@users.noreply.github.com>
2023-11-09 10:26:29 +0100

    Added terraform cloud workspace and organization

--- backend.tf
+++ backend.tf
@@ -1,8 +1,7 @@
 terraform {
   required_version = ">= 1.1.0"
-  backend "remote" {
-    hostname = "app.terraform.io"
-    # organization = "<replace-with-your-Terraform-Cloud-organization-and-uncomment>"
+  cloud {
+    organization = "Snort"
     workspaces {
       name = "ace-iac-day-zero"
     }
